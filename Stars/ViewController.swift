1e1a2520f12504d17c2f164c539649d1c2457f97
Craig Swanson <cswanson02@me.com>
2019-10-03 20:36:52 -0500

    Initial Commit

---
+++ Stars/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Stars
+//
+//  Created by Craig Swanson on 10/3/19.
+//  Copyright © 2019 Craig Swanson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
