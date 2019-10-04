2618eea3800cf7a0789702a78024eef0b9a93ad8
Craig Swanson <cswanson02@me.com>
2019-10-03 21:12:40 -0500

    Hooked up outlets and actions to the view controller

--- Stars/StarsViewController.swift
+++ Stars/StarsViewController.swift
@@ -10,11 +10,21 @@ import UIKit
 
 class StarsViewController: UIViewController {
 
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var distanceTextField: UITextField!
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func printStars(_ sender: UIButton) {
+        print("Tapped on print stars")
+    }
+    
+    @IBAction func createStar(_ sender: UIButton) {
+        print("Tapped on create a star")
+    }
 }
 
