07c578acf559b94cd06a951e57d3280aedeed55a
Craig Swanson <cswanson02@me.com>
2019-10-03 22:43:57 -0500

    added saving and loading

--- Stars/StarModelController.swift
+++ Stars/StarModelController.swift
@@ -13,10 +13,23 @@ class StarModelController {
     // Also, when empty parentheses are after a type, it creates a new instance of that type -- eg:   var starA = [Star]() would work just as well
     private(set) var stars: [Star] = []
     
+    //when a star model is created (above line), it loads the data
+    
+    init() {
+        loadFromPersistentStore()
+    }
+    
+    private var persistentFileURL: URL? {
+        let fileManager = FileManager.default
+        guard let documents = try? fileManager.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: true) else { return nil }
+        return documents.appendingPathComponent("stars.plist")
+    }
+    
     func createStar(named name: String, withDistance distance: Double) {
         let star = Star(name: name, distanceInLightYears: distance)
         
         stars.append(star)
+        saveToPersistentStore()
     }
     
     func listStars() -> String {
@@ -25,5 +38,28 @@ class StarModelController {
             output += "\(star.name) is \(star.distanceDescription).\n"
         }
         return output
+    }
+    
+    func saveToPersistentStore() {
+        guard let url = persistentFileURL else { return }
+        
+        do {
+        let encoder = PropertyListEncoder()
+        let data = try encoder.encode(stars)
+        } catch {
+         print("Error saving stars data: \(error)")
+        }
+    }
+    
+    func loadFromPersistentStore() {
+        guard let url = persistentFileURL else { return }
+        
+        do {
+            let data = try Data(contentsOf: url)
+            let decoder = PropertyListDecoder()
+            stars = try decoder.decode([Star].self, from: data)
+        } catch {
+            print("Error loading stars data: \(error)")
+        }
     }
 }
