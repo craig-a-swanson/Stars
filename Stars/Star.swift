2c82d20c9629bc7ecbaf5b67a91870b114220914
Craig Swanson <cswanson02@me.com>
2019-10-03 21:26:54 -0500

    Created model controller

---
+++ Stars/Star.swift
@@ -0,0 +1,14 @@
+//
+//  Star.swift
+//  Stars
+//
+//  Created by Craig Swanson on 10/3/19.
+//  Copyright © 2019 Craig Swanson. All rights reserved.
+//
+
+import Foundation
+
+struct Star {
+    var name: String
+    var distanceInLightYears: Double
+}
---
+++ Stars/StarModelController.swift
@@ -0,0 +1,21 @@
+//
+//  StarModelController.swift
+//  Stars
+//
+//  Created by Craig Swanson on 10/3/19.
+//  Copyright © 2019 Craig Swanson. All rights reserved.
+//
+
+import Foundation
+
+class StarController {
+    // by marking the setter as private, means that nothing else can enter things into this array
+    // Also, when empty parentheses are after a type, it creates a new instance of that type -- eg:   var starA = [Star]() would work just as well
+    private(set) var stars: [Star] = []
+    
+    func createStar(named name: String, withDistance distance: Double) {
+        let star = Star(name: name, distanceInLightYears: distance)
+        
+        stars.append(star)
+    }
+}
